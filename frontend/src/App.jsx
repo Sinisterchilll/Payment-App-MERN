312e2d428bb3ff3e9d8e460aba5289e4c0fdfbd4
Sinisterchilll <anucbs22@gmail.com>
2024-03-01 16:15:03 +0530

    Added frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
-import { BrowserRouter , Routes , Route } from "react-router-dom"
-function App() {
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Signup } from "./Pages/Signup";
+import { Signin } from "./Pages/Signin";
+import { Dashboard } from "./Pages/Dashboard";
+import { SendMoney } from "./Pages/SendMoney";
 
+function App() {
   return (
-    <div>
-        <BrowserRouter>
+    <>
+       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />}/>
-          <Route path="/signin" element={<Signin />}/>
-          <Route path="/dashboard" element={<Dashboard />}/>
-          <Route path="/send" element={<Sendmoney />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
         </Routes>
-        </BrowserRouter>
-    </div>
+      </BrowserRouter>
+    </>
   )
 }
 
